0987f2bab105e5421b56b1bb18561e4b13dee382
Sheogorath <sheogorath@shivering-isles.de>
2015-09-26 03:49:36 +0200

    some improvements to fix path issues

---
+++ config.example.php
@@ -0,0 +1,8 @@
+<?php
+$NODELIST = 'https://meshviewer.fulda.freifunk.net/data/nodes.json';
+$APILISTFILE = 'https://github.com/freifunk/directory.api.freifunk.net/raw/master/directory.json';
+$APIOUTPUTFILE = __DIR__ .'/api.json';
+$APIMINOUTPUTFILE = __DIR__ .'/api.min.json';
+$COMMUNITY = 'fulda';
+
+?>
--- ff-api-update.php
+++ ff-api-update.php
@@ -6,27 +6,41 @@
  * written to the freifunk.net API file.
  */
  
-$NODELIST = 'https://meshviewer.fulda.freifunk.net/data/nodes.json';
-$APIFILE = '/path/to/FreifunkFulda-api.json';
- 
-$map_json = file_get_contents($NODELIST);
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);
+
+require (__DIR__ . '/config.php');
+
+$list_json = file_get_contents($APILISTFILE, false, stream_context_create($arrContextOptions));
+$list_php = json_decode($list_json);
+
+$apifile = $list_php->{$COMMUNITY};
+
+$map_json = file_get_contents($NODELIST, false, stream_context_create($arrContextOptions));
 $map_php = json_decode($map_json);
- 
+
 $nodes = count((array)($map_php->{'nodes'}));
- 
+
 foreach($map_php->{'nodes'} as $node) {
     if ($node->{'flags'}->{'gateway'} == true)
         $nodes = $nodes -1;
     else if ($node->{'flags'}->{'online'} == false)
         $nodes = $nodes -1;
 }
- 
-$api_json = file_get_contents ($APIFILE);
+
+$api_json = file_get_contents ($apifile, false, stream_context_create($arrContextOptions));
 $json_php = json_decode($api_json);
- 
+
 $json_php->{'state'}->{'nodes'} = $nodes;
- 
+
+$api_json = json_encode($json_php, JSON_PRETTY_PRINT);
+$json_string = file_put_contents ($APIOUTPUTFILE, $api_json);
+
 $api_json = json_encode($json_php);
-$json_string = file_put_contents ($APIFILE, $api_json);
- 
+$json_string = file_put_contents ($APIMINOUTPUTFILE, $api_json);
+
 ?>
